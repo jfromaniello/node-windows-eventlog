736550a46ab36cb0f3e28a596e4209053d474a0d
Jose F. Romaniello <jfromaniello@gmail.com>
2012-04-09 10:14:27 -0300

    first code

---
+++ EventLog/EventLog.cpp
@@ -0,0 +1,119 @@
+#pragma comment(lib, "node")
+
+#using <mscorlib.dll>
+#using <system.dll>
+
+#include <node.h>
+#include <v8.h>
+#include <string>
+#include <gcroot.h>
+#include <string>
+#include <iostream>
+#include <uv.h>
+
+using namespace node;
+using namespace v8;
+
+class EventLog : ObjectWrap
+{
+private:
+	gcroot<System::Diagnostics::EventLog^> _eventLog;
+
+public:
+
+    static Persistent<FunctionTemplate> s_ct;
+    static void NODE_EXTERN Init(Handle<Object> target)
+    {
+        HandleScope scope;
+
+        // set the constructor function
+        Local<FunctionTemplate> t = FunctionTemplate::New(New);
+
+        // set the node.js/v8 class name
+        s_ct = Persistent<FunctionTemplate>::New(t);
+        s_ct->InstanceTemplate()->SetInternalFieldCount(1);
+        s_ct->SetClassName(String::NewSymbol("EventLog"));
+
+        // registers a class member functions 
+        NODE_SET_PROTOTYPE_METHOD(s_ct, "log", Log);
+        
+        target->Set(String::NewSymbol("EventLog"),
+            s_ct->GetFunction());
+    }
+
+    EventLog() 
+    {}
+
+    ~EventLog()
+    {}
+
+	
+	static inline gcroot<System::String^> ParseArgument(Arguments const&args, int argumentIndex)
+	{
+		Local<String> message = Local<String>::Cast(args[argumentIndex]);
+		gcroot<System::String^> m = gcnew System::String(((std::string)*v8::String::AsciiValue(message)).c_str());
+		return m;
+	}
+
+    static Handle<Value> New(const Arguments& args)
+    {
+		HandleScope scope;
+
+		if (!args[0]->IsString()) {
+		    return ThrowException(Exception::TypeError(
+		        String::New("First argument must be the name of the event log source")));
+		}
+		if (!args[1]->IsString()) {
+		    return ThrowException(Exception::TypeError(
+		        String::New("Second argument must be the name of the event log: (Application, System)")));
+		}
+
+		System::String^ s = ParseArgument(args, 0);
+		System::String^ ln = ParseArgument(args, 1);
+
+		if(!System::Diagnostics::EventLog::SourceExists(s)){
+			System::Diagnostics::EventLog::CreateEventSource(s, ln);
+		}
+        
+		EventLog* pm = new EventLog();
+		pm->_eventLog = gcnew System::Diagnostics::EventLog();
+		pm->_eventLog->Source = s;
+
+        pm->Wrap(args.This());
+        return args.This();
+    }
+
+
+	static Handle<Value> Log(const Arguments& args)
+    {
+		if (!args[0]->IsString()) {
+		    return ThrowException(Exception::TypeError(
+		        String::New("First argument must be the message to log.")));
+		}
+		
+		if (!args[1]->IsString()) {
+		    return ThrowException(Exception::TypeError(
+		        String::New("Second argument must be the type of the entry Information/Warning/Error.")));
+		}
+
+		gcroot<System::String^> m = ParseArgument(args, 0);
+		gcroot<System::String^> t = ParseArgument(args, 1);
+		gcroot<System::Diagnostics::EventLogEntryType> logt = (System::Diagnostics::EventLogEntryType)System::Enum::Parse(System::Diagnostics::EventLogEntryType::typeid, t);
+
+		EventLog* xthis = ObjectWrap::Unwrap<EventLog>(args.This());
+
+		xthis->_eventLog->WriteEntry(m, logt, 1000);
+
+	    return Undefined();
+    }
+};
+
+Persistent<FunctionTemplate> EventLog::s_ct;
+
+extern "C" {
+    void NODE_EXTERN init (Handle<Object> target)
+    {
+        EventLog::Init(target);
+    }
+    NODE_MODULE(sharp, init);
+}
---
+++ README.md
@@ -0,0 +1,62 @@
+
+# WinLog
+
+  Windows Event Log logger for node.js [Winston](https://github.com/flatiron/winston) module.
+
+## Installation
+
+    $ npm install windows-eventlog
+
+## Usage
+
+Initialize somewhere the logger like:
+
+```js
+  var eventlog = require('windows-eventlog');
+  eventlog.init("MyAppName");
+```
+
+Then you can do:
+
+```js
+  var eventlog = require('windows-eventlog');
+  eventlog.log("This is an information messsage");
+  eventlog.log("This is another info message", "Information");
+  eventlog.log("This is an error message", "Error");
+  eventlog.log("This is a warning", "Warning");
+```
+
+And you will see this:
+
+![2012-04-09_1007.png](http://joseoncodecom.ipage.com/wp-content/uploads/images/2012-04-09_1007.png)
+
+## How it works
+
+This module was built on c++/cli (.Net) and uses [System.Diagnostics.EventLog](http://msdn.microsoft.com/en-us/library/system.diagnostics.eventlog.aspx). 
+
+In order to log events you need to run the application with an elevated account: ie administrator or system account. Windows services run with the system account so this project works  well with [WinSer](https://github.com/jfromaniello/winser).
+
+## License 
+
+(The MIT License)
+
+Copyright (c) 2012 Jose Romaniello &lt;jfromaniello@gmail.com&gt;
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+'Software'), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
