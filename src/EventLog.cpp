c79331131534b55d2ecda83bf3cb8908c15181d4
Maximilian Haupt <mail@maximilianhaupt.com>
2016-02-22 21:51:28 +0100

    Port EventLog to Nan.
    This way almost all node.js version are supported.

--- src/EventLog.cpp
+++ src/EventLog.cpp
@@ -4,133 +4,135 @@
 #using <system.dll>
 
 #include <node.h>
+#include <nan.h>
 #include <v8.h>
 #include <string>
 #include <gcroot.h>
 #include <string>
 #include <iostream>
-#include <uv.h>
 #include <vcclr.h>
+#include <msclr\marshal_cppstd.h>
 
-using namespace node;
-using namespace v8;
-
-class EventLog : ObjectWrap
+class EventLog : Nan::ObjectWrap
 {
 private:
-	gcroot<System::Diagnostics::EventLog^> _eventLog;
+    gcroot<System::Diagnostics::EventLog^> _eventLog;
 
 public:
-
-    static Persistent<FunctionTemplate> s_ct;
-    static void Init(Handle<Object> target)
+    static NAN_MODULE_INIT(Init)
     {
-        HandleScope scope;
+        v8::Local<v8::FunctionTemplate> tpl = Nan::New<v8::FunctionTemplate>(New);
+        tpl->SetClassName(Nan::New("EventLog").ToLocalChecked());
+        tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-        // set the constructor function
-        Local<FunctionTemplate> t = FunctionTemplate::New(New);
+        SetPrototypeMethod(tpl, "log", Log);
 
-        // set the node.js/v8 class name
-        s_ct = Persistent<FunctionTemplate>::New(t);
-        s_ct->InstanceTemplate()->SetInternalFieldCount(1);
-        s_ct->SetClassName(String::NewSymbol("EventLog"));
-
-        // registers a class member functions 
-        NODE_SET_PROTOTYPE_METHOD(s_ct, "log", Log);
-        
-        target->Set(String::NewSymbol("EventLog"),
-            s_ct->GetFunction());
+        constructor().Reset(Nan::GetFunction(tpl).ToLocalChecked());
+        Nan::Set(target, Nan::New("EventLog").ToLocalChecked(),
+            Nan::GetFunction(tpl).ToLocalChecked());
     }
 
-    EventLog(System::String^ source, System::String^ logName) 
+    EventLog(System::String^ source, System::String^ logName)
     {
-		if(!System::Diagnostics::EventLog::SourceExists(source)){
-			System::Diagnostics::EventLog::CreateEventSource(source, logName);
-		}
-        
-		_eventLog = gcnew System::Diagnostics::EventLog();
-		_eventLog->Source = source;
-	}
+        if (!System::Diagnostics::EventLog::SourceExists(source)) {
+            System::Diagnostics::EventLog::CreateEventSource(source, logName);
+        }
+
+        _eventLog = gcnew System::Diagnostics::EventLog();
+        _eventLog->Source = source;
+    }
 
     ~EventLog()
     {
-		delete _eventLog;
-	}
-
-	
-	static inline gcroot<System::String^> ParseArgument(Arguments const&args, int argumentIndex)
-	{
-		return gcnew System::String((const wchar_t *) *v8::String::Value(args[argumentIndex]));
-	}
-
-	static std::wstring StringToWstring(System::String^ s)
-	{
-		pin_ptr<const wchar_t> wch = PtrToStringChars(s);
-		return std::wstring(wch);
-	}
-
-    static Handle<Value> New(const Arguments& args)
-    {
-	HandleScope scope;
-
-	if (!args[0]->IsString()) {
-	    return ThrowException(Exception::TypeError(
-	        String::New("First argument must be the name of the event log source")));
-	}
-	
-	try
-	{
-		System::String^ s = ParseArgument(args, 0);
-		System::String^ ln;
-	
-		if (!args[1]->IsString()) {
-			ln = "Application";
-		}else{
-			ln = ParseArgument(args, 1);
-		}
-	
-		EventLog* pm = new EventLog(s, ln);
-	
-	        pm->Wrap(args.This());
-	        return args.This();
-	}
-	catch (System::Exception^ e)
-	{
-		return ThrowException(Exception::Error(String::New((uint16_t *) (StringToWstring(e->Message)).c_str())));
-	}
+        delete _eventLog;
     }
 
-
-	static Handle<Value> Log(const Arguments& args)
+    static inline gcroot<System::String^> ParseArgument(const Nan::FunctionCallbackInfo<v8::Value>& args, int argumentIndex)
     {
-		if (!args[0]->IsString()) {
-		    return ThrowException(Exception::TypeError(
-		        String::New("First argument must be the message to log.")));
-		}
-		gcroot<System::String^> m = ParseArgument(args, 0);
-		gcroot<System::String^> t;
-		
-
-		if (!args[1]->IsString()) {
-		    t = "Information";
-		}else{
-			t = ParseArgument(args, 1);
-		}
-
-		gcroot<System::Diagnostics::EventLogEntryType> logt = (System::Diagnostics::EventLogEntryType)System::Enum::Parse(System::Diagnostics::EventLogEntryType::typeid, t);
-
-		EventLog* xthis = ObjectWrap::Unwrap<EventLog>(args.This());
-
-		xthis->_eventLog->WriteEntry(m, logt, 1000);
-
-	    return Undefined();
+        Nan::Utf8String arg(args[argumentIndex]->ToString());
+        std::string stdArg = *arg;
+        std::wstring wideArg(stdArg.begin(), stdArg.end());
+        return gcnew System::String(wideArg.c_str());
     }
+
+    static std::string MarshalString(System::String ^ s)
+    {
+        msclr::interop::marshal_context context;
+        return context.marshal_as<std::string>(s);
+    }
+
+    static NAN_METHOD(New) {
+        if (!info.IsConstructCall()) {
+            const int argc = 2;
+            v8::Local <v8::Value> argv[argc] = { info[0], info[1] };
+            v8::Local <v8::Function> cons = Nan::New(constructor());
+            info.GetReturnValue().Set(cons->NewInstance(argc, argv));
+            return;
+        }
+
+        if (!info[0]->IsString()) {
+            Nan::ThrowError("First argument must be the name of the event log source");
+            return;
+        }
+
+        try
+        {
+            System::String^ s = ParseArgument(info, 0);
+            System::String^ ln;
+
+            if (!info[1]->IsString()) {
+                ln = "Application";
+            }
+            else {
+                ln = ParseArgument(info, 1);
+            }
+
+            EventLog* pm = new EventLog(s, ln);
+
+            pm->Wrap(info.This());
+            info.GetReturnValue().Set(info.This());
+        }
+        catch (System::Exception^ e)
+        {
+            Nan::ThrowError(MarshalString(e->Message).c_str());
+        }
+    }
+
+    static void Log(const Nan::FunctionCallbackInfo<v8::Value>& info)
+    {
+        if (!info[0]->IsString()) {
+            Nan::ThrowTypeError("First argument must be the message to log.");
+            return;
+        }
+
+        gcroot<System::String^> m = ParseArgument(info, 0);
+        gcroot<System::String^> t;
+
+        if (!info[1]->IsString()) {
+            t = "Information";
+        }
+        else {
+            t = ParseArgument(info, 1);
+        }
+
+        gcroot<System::Diagnostics::EventLogEntryType> logt = (System::Diagnostics::EventLogEntryType)System::Enum::Parse(System::Diagnostics::EventLogEntryType::typeid, t);
+
+        EventLog* xthis = ObjectWrap::Unwrap<EventLog>(info.This());
+
+        xthis->_eventLog->WriteEntry(m, logt, 1000);
+
+        //return Undefined();
+    }
+
+    static inline Nan::Persistent<v8::Function>& constructor() {
+        static Nan::Persistent<v8::Function> my_constructor;
+        return my_constructor;
+    }
+
 };
 
-Persistent<FunctionTemplate> EventLog::s_ct;
-
 extern "C" {
-    void init (v8::Handle<v8::Object> target)
+    void init(v8::Handle<v8::Object> target)
     {
         EventLog::Init(target);
     }
