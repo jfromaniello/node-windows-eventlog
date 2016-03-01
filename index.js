0abb776b518ad2f4f1e6c7cb8258723f1dd036f6
Maximilian Haupt <mail@maximilianhaupt.com>
2016-03-01 19:28:34 +0100

    Remove prebuilt binaries.
    Also use the `bindings` node module to locate the correct binary instead
    of doing it manually.

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var fs = require('fs');
-var path = require('path');
-
-var gypRequirePathname = './build/Release/EventLog.node';
-if (fs.existsSync(path.normalize(module.filename + '/../' + gypRequirePathname))) {
-  module.exports = require(gypRequirePathname);
-} else {
-  module.exports = require('./prebuild/' + process.arch + '/EventLog.node');
-}
+module.exports = require('bindings')('EventLog');
